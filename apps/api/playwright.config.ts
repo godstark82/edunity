2b58bc8c6238432622e996275d14763af3e36884
Lalit Sharma <ls8290519977@gmail.com>
2025-09-11 03:27:28 +0530

    added testing

---
+++ apps/api/playwright.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  retries: 1,
+  
+  use: {
+    baseURL: 'http://localhost:3000',
+    headless: true,
+  },
+  reporter: [['list'], ['html', { open: 'never' }]],
+});
