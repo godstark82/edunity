4ea8be7cd528eb3f821fabd90e67b19d863602d9
Lalit Sharma <ls8290519977@gmail.com>
2025-09-17 19:34:38 +0530

    added supabase and other helper functions

--- apps/api/app/route.ts
+++ apps/api/app/route.ts
@@ -1,13 +1,16 @@
 import { NextResponse } from "next/server";
 import { errorResponse, successResponse } from "../utils/response-wraper";
+import { createServerSideClient } from "@edunity/supabase"
 
 export async function GET() {
-  try { 
-  const result = {
-    message: "Welcome to Edunity API",
-    timestamp: new Date().toISOString(),
-  };  
-  return successResponse(result);
-  } catch (error:any) {
+  try {
+    const supabase = createServerSideClient()
+    const result = {
+      message: "Welcome to Edunity API",
+      timestamp: new Date().toISOString(),
+    };
+    return successResponse(result);
+  } catch (error: any) {
     return errorResponse(error.message || "Internal Server Error", 500);
-}}  
+  }
+}  
---
+++ packages/supabase/index.ts
@@ -0,0 +1,4 @@
+export * from "./types";
+export * from "./clients/client"
+export * from "./clients/middleware"
+export * from "./clients/server"
